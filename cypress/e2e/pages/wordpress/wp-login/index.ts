766d312042c6fff4a89d00b5b753df6e4275c0fb
wambookontakt@gmail.com <wambookontakt@gmail.com>
2023-12-14 00:01:58 +0000

    Login error assert test

--- cypress/e2e/pages/wordpress/wp-login/index.ts
+++ cypress/e2e/pages/wordpress/wp-login/index.ts
@@ -1,3 +1,4 @@
 export * from './wp-login.page.selectors';
 export * from './wp-login.page.actions';
 export * from './wp-login.page.constants';
+export * from './wp-login.page.asserts';
--- cypress/e2e/test-suites/wordpress/wp-login/log-in-to-admin-panel.cy.ts
+++ cypress/e2e/test-suites/wordpress/wp-login/log-in-to-admin-panel.cy.ts
@@ -1,8 +1,13 @@
-import { WP_LOG_IN_PAGE_URL, WpLoginActions } from '../../../pages/wordpress/wp-login';
+import {
+    WP_LOG_IN_PAGE_URL,
+    WpLoginActions,
+    WpLoginAsserts,
+} from '../../../pages/wordpress/wp-login';
 import { WP_ADMIN, WP_ADMIN_PASSWORD, WP_USER1, WP_USER1_PASSWORD } from '../../../../.config';
 import { WpAdminBarAsserts } from '../../../components/wordpress/wp-admin-bar';
 
 const wpLoginActions: WpLoginActions = new WpLoginActions();
+const wpLoginAsserts: WpLoginAsserts = new WpLoginAsserts();
 const wpAdminBarAsserts: WpAdminBarAsserts = new WpAdminBarAsserts();
 
 describe('Wordpress log in website - user log in options', () => {
@@ -17,5 +22,6 @@ describe('Wordpress log in website - user log in options', () => {
 
     it('User can not log in using wordpress login form, the wrong user should display the correct error label.', () => {
         wpLoginActions.logIn(WP_USER1, WP_USER1_PASSWORD);
+        wpLoginAsserts.verifyLoginErrorLabel(WP_USER1);
     });
 });
