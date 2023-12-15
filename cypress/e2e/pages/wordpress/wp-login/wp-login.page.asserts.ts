6d2b401ccbec7016b7f8967805dfc2f66b7d75fc
wambookontakt@gmail.com <wambookontakt@gmail.com>
2023-12-15 23:29:50 +0000

    Assert info update

--- cypress/e2e/pages/wordpress/wp-login/wp-login.page.asserts.ts
+++ cypress/e2e/pages/wordpress/wp-login/wp-login.page.asserts.ts
@@ -3,10 +3,10 @@ import { wpLoginSelectors } from './wp-login.page.selectors';
 export class WpLoginAsserts {
     verifyLoginErrorLabel(labelUser: string | undefined): void {
         if (typeof labelUser !== 'string')
-            throw new Error('Environment variable for parameter: labelUser has not been set');
+            throw new Error('Environment variable for parameter: labelUser has not been set.');
 
         if (labelUser.length === 0)
-            throw new Error('Parameter: labelUser should not be an empty string');
+            throw new Error('Parameter: labelUser should not be an empty string.');
 
         cy.get(wpLoginSelectors.loginForm.loginErrorLabel).should('contain.text', labelUser);
     }
