113f5816d5d3d62cc0b034a7fbc2716b608714a8
wambookontakt@gmail.com <wambookontakt@gmail.com>
2024-01-12 15:39:41 +0000

    timeout update

--- cypress/e2e/components/wordpress/wp-admin-bar/wp-admin-bar.component.asserts.ts
+++ cypress/e2e/components/wordpress/wp-admin-bar/wp-admin-bar.component.asserts.ts
@@ -2,6 +2,6 @@ import { wpAdminBarSelectors } from './wp-admin-bar.component.selectors';
 
 export class WpAdminBarAsserts {
     verifyAdminPanelHelpLinkExists(): void {
-        cy.get(wpAdminBarSelectors.adminPanelHelpLink, { timeout: 10000 }).should('exist').should('be.visible');
+        cy.get(wpAdminBarSelectors.adminPanelHelpLink, { timeout: 20000 }).should('exist').should('be.visible');
     }
 }
