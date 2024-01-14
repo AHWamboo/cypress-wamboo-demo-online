57393b40e585ff53304af22ca6330fac8ff1d1ca
wambookontakt@gmail.com <wambookontakt@gmail.com>
2024-01-14 23:27:05 +0000

    CI test check

--- cypress/e2e/test-suites/wordpress/wp-login/log-in-to-admin-panel.aa.ts
+++ cypress/e2e/test-suites/wordpress/wp-login/log-in-to-admin-panel.aa.ts
@@ -1,20 +1,20 @@
 import { WP_LOG_IN_PAGE_URL, WpLoginActions, WpLoginAsserts } from '../../../pages/wordpress/wp-login';
-import { WP_USER1, WP_USER1_PASSWORD } from '../../../../.config';
-// import { WpAdminBarAsserts } from '../../../components/wordpress/wp-admin-bar';
+import { WP_ADMIN, WP_ADMIN_PASSWORD, WP_USER1, WP_USER1_PASSWORD } from '../../../../.config';
+import { WpAdminBarAsserts } from '../../../components/wordpress/wp-admin-bar';
 
 const wpLoginActions: WpLoginActions = new WpLoginActions();
 const wpLoginAsserts: WpLoginAsserts = new WpLoginAsserts();
-// const wpAdminBarAsserts: WpAdminBarAsserts = new WpAdminBarAsserts();
+const wpAdminBarAsserts: WpAdminBarAsserts = new WpAdminBarAsserts();
 
 describe('Wordpress log in website - user log in options', () => {
     beforeEach(() => {
         cy.visit(WP_LOG_IN_PAGE_URL);
     });
 
-    // it('User can log in using the wordpress login form', () => {
-    //     wpLoginActions.logIn(WP_ADMIN, WP_ADMIN_PASSWORD);
-    //     wpAdminBarAsserts.verifyAdminPanelHelpLinkExists();
-    // });
+    it('User can log in using the wordpress login form', () => {
+        wpLoginActions.logIn(WP_ADMIN, WP_ADMIN_PASSWORD);
+        wpAdminBarAsserts.verifyAdminPanelHelpLinkExists();
+    });
 
     it('User can not log in using wordpress login form, the wrong user should display the correct error label.', () => {
         wpLoginActions.logIn(WP_USER1, WP_USER1_PASSWORD);
